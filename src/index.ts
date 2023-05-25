dc792c86b0f27b0e824845068ca683725eda9260
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-05-25 20:38:18 -0300

    configuracao-api-concluida

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(Number(process.env.PORT) || 3003, () => {
+    console.log(`Servidor rodando na porta ${Number(process.env.PORT) || 3003}`)
+})
+
+// routers das entidades
+
+app.get("/ping", (req, res) => {
+    res.send("Pong!")
+})
