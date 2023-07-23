e53cca6be8f040117e61aef1354a6e2897f5f2bb
Samuel Pereira <samuel_sogas@yahoo.com.br>
2023-07-23 20:35:03 -0300

    Criando database e preparando express e knex

---
+++ src/database/labook.sql
@@ -0,0 +1,30 @@
+-- Active: 1690154905379@@127.0.0.1@3306
+CREATE TABLE if NOT EXISTS users(
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    name TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL,
+    created_at TEXT DEFAULT (DATETIME('now')) NOT NULL 
+);
+CREATE TABLE if NOT EXISTS posts(    
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    creator_id TEXT UNIQUE NOT NULL,
+    content TEXT NOT NULL,
+    likes INTEGER NOT NULL,
+    dislikes INTEGER NOT NULL,
+    created_at TEXT DEFAULT (DATETIME('now')) NOT NULL ,
+    updated_at TEXT DEFAULT (DATETIME('now')) NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES users(id)  
+    ON UPDATE CASCADE
+    ON DELETE CASCADE  
+);
+CREATE TABLE if NOT EXISTS likes_dislikes(    
+    user_id TEXT UNIQUE NOT NULL,
+    post_id TEXT UNIQUE NOT NULL,
+    like INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id), 
+    FOREIGN KEY (post_id) REFERENCES posts(id)  
+    ON UPDATE CASCADE
+    ON DELETE CASCADE   
+);
