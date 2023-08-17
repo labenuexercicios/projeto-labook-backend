43d24951aaa66f54b1e4316d283e25703c86b056
Samuel Pereira <samuel_sogas@yahoo.com.br>
2023-08-17 03:34:08 -0300

    Dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import { userRouter } from './router/userRouter'
 import { postRouter } from './router/postRouter'
 
+dotenv.config()
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(3003, () => {
-    console.log(`Servidor rodando na porta ${3003}`)
+app.listen(Number(process.env.PORT || 3003), () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT}`)
 })
-
 
 app.use("/users", userRouter)
 app.use("/posts", postRouter)
