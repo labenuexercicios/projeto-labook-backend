4ffc56f65605b2fa82094041fef95026ea47dce8
Samuel Pereira <108569272+eu-samuel@users.noreply.github.com>
2023-09-17 15:58:39 -0300

    Update getPosts.dto.ts

--- src/dtos/Posts/getPosts.dto.ts
+++ src/dtos/Posts/getPosts.dto.ts
@@ -2,11 +2,7 @@ import z from "zod"
 import { PostModel } from "../../models/Post"
 
 export interface GetPostsInputDTO {
-  token: string 
-}
-
-export interface GetPostsByContentInputDTO {
-  content: string,
+  q?: string,
   token: string 
 }
 
@@ -15,21 +11,10 @@ export interface GetPostByIdInputDTO {
   token: string 
 }
 
-export interface GetUserPostsInputDTO {
-  creatorId: string,
-  token: string 
-}
-
 export const GetPostsSchema = z.object({
+  q: z.string().min(1).optional(),
   token: z.string().min(1)
 }).transform(data => data as GetPostsInputDTO)
-
-
-export const GetPostsByContentSchema = z.object({
-  content: z.string().min(1),
-  token: z.string().min(1)
-}).transform(data => data as GetPostsByContentInputDTO)
-
 
 export const GetPostByIdSchema = z.object({
   id: z.string().min(1),
@@ -37,11 +22,4 @@ export const GetPostByIdSchema = z.object({
 }).transform(data => data as GetPostByIdInputDTO)
 
 
-export const GetUserPostsSchema = z.object({
-  creatorId: z.string().min(1),
-  token: z.string().min(1)
-}).transform(data => data as GetUserPostsInputDTO)
-
-export type GetPostsOutputDTO = PostModel[]
-export type GetSinglePostOutputDTO = PostModel
-export type GetUserPostsOutputDTO = PostModel[]
+export type GetPostsOutputDTO = PostModel[] | PostModel
