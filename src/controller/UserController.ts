72a0d2e9cabbcb3392122142cd46eaca3d745182
Samuel Pereira <108569272+eu-samuel@users.noreply.github.com>
2023-09-17 15:46:40 -0300

    Update UserController.ts

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -16,7 +16,7 @@ export class UserController {
     public getUsers = async (req: Request, res: Response) => {
         try {
           const input = GetUsersSchema.parse({
-            name: req.query.name,
+            q: req.query.name,
             token: req.headers.authorization
         })
 
@@ -35,30 +35,6 @@ export class UserController {
             }
         }
     }
-
-    
-    public getUserByName = async (req: Request, res: Response) => {
-      try {
-        const input = GetUsersSchema.parse({
-          name: req.params.name,
-          token: req.headers.authorization
-      })
-
-            const output = await this.userBusiness.getUserByName(input)
-            res.status(200).send(output)
-
-      } catch (error) {
-          console.log(error)
-
-          if (error instanceof ZodError) {
-              res.status(400).send(error.issues)
-          } else if (error instanceof BaseError) {
-              res.status(error.statusCode).send(error.message)
-          } else {
-              res.status(500).send("Erro inesperado")
-          }
-      }
-  }
   
     public signup = async (req: Request, res: Response) => {
 
