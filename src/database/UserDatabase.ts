0fc2f541816034fe4ab1ccacae1d27b46ef5bc90
Bernardo <vargasbernardo97@gmail.com>
2023-10-28 17:12:23 -0300

    feat: criacao dos arquivos de database e conexao com knex

---
+++ src/database/UserDatabase.ts
@@ -0,0 +1,6 @@
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+    public static TABLE_USERS = 'users'
+    
+}
--- src/index.ts
+++ src/index.ts
@@ -15,24 +15,3 @@ app.listen('3003', () => {
 app.get('/ping', (req: Request, res: Response) => {
     res.status(200).send('pong')
 })
-
-const post = new Post(
-    'u001',
-    'u003',
-    'Coe irmao',
-    5,
-    0,
-    new Date().toISOString(),
-    new Date().toISOString()
-)
-console.log(post)
-
-const user = new User(
-    'u003',
-    'fulanoi',
-    'fulano@email',
-    'fluano123',
-    'friend',
-    new Date().toISOString()
-)
-console.log(user)
