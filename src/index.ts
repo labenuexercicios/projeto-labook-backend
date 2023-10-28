8b07a38b9a14f28ee9a6ca1dcd900e516ff6c624
Bernardo <vargasbernardo97@gmail.com>
2023-10-28 17:01:35 -0300

    feat: models para user e posts criados

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response} from 'express'
 import cors from 'cors'
+import { Post } from './models/Post'
+import { User } from './models/User'
 
 const app = express()
 app.use(express.json())
@@ -13,3 +15,24 @@ app.listen('3003', () => {
 app.get('/ping', (req: Request, res: Response) => {
     res.status(200).send('pong')
 })
+
+const post = new Post(
+    'u001',
+    'u003',
+    'Coe irmao',
+    5,
+    0,
+    new Date().toISOString(),
+    new Date().toISOString()
+)
+console.log(post)
+
+const user = new User(
+    'u003',
+    'fulanoi',
+    'fulano@email',
+    'fluano123',
+    'friend',
+    new Date().toISOString()
+)
+console.log(user)
