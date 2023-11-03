9ec7f62c21ff1833dfadf849ed5388bfda8d1daa
anderson-nunes <andersonribeironunes@hotmail.com>
2023-11-03 10:28:13 -0300

    carried out the initial project configurations

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3003, () => {
+  console.log(`Servidor rodando na porta ${3003}`);
+});
+
+app.get("/ping", async (req: Request, res: Response) => {
+  try {
+    res.status(200).send({ message: "Pong!" });
+  } catch (error) {
+    console.log(error);
+
+    if (req.statusCode === 200) {
+      res.status(500);
+    }
+
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.send("Erro inesperado");
+    }
+  }
+});
