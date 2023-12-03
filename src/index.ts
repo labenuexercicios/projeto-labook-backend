ff329b045e6f8ec1c2c11021ee6d83e0a1117198
Re-Santos <renata.prog@gmail.com>
2023-12-03 14:55:24 -0300

    configuração inicial

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors'
+import { db } from './database/knex'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003")
+});
+
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Pong!')
+});
