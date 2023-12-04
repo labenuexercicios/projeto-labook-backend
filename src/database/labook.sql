f52fc8cf727dd53483cc3bbf18114b1f70ea182f
Re-Santos <renata.prog@gmail.com>
2023-12-04 15:50:09 -0300

    Configurei as models

--- src/database/labook.sql
+++ src/database/labook.sql
@@ -3,7 +3,7 @@
 
 PRAGMA foreign_keys=on;
 
-
+--Ainda tenho duvidas sobre o quesito role, por isso ele ainda não consta na tabela.
 CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE users (
     created_at TEXT DEFAULT (DATETIME())
 );
 
-DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS users;
 
 INSERT INTO users (id, name, email, password) 
 VALUES ('u001', 'Fulano', 'fulano@email.com', 'fulano123'),
@@ -22,9 +22,10 @@ VALUES ('u001', 'Fulano', 'fulano@email.com', 'fulano123'),
 
 SELECT * FROM users;
 
+
 CREATE TABLE posts (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
-    creator_id TEXT NOT NULL REFERENCES users (id),
+    creator_id TEXT NOT NULL REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
     content TEXT NOT NULL,
     likes INTEGER NOT NULL,
     dislikes INTEGER NOT NULL,
@@ -33,10 +34,15 @@ CREATE TABLE posts (
     FOREIGN KEY (creator_id) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS posts;
+
+-- DROP TABLE IF EXISTS posts;
 
 INSERT INTO posts (id, creator_id, content, likes, dislikes, updated_at)
-VALUES ('1', 'u003', 'postagem 1', 10, 2, (DATETIME()));
+VALUES 
+  ('p001', 'u001', 'Postagem do usuário u001', 5, 1, DATETIME('now')),
+  ('p002', 'u002', 'Postagem do usuário u002', 8, 3, DATETIME('now')),
+  ('p003', 'u003', 'Postagem do usuário u003', 12, 2, DATETIME('now'));
+
 
 SELECT * FROM posts;
 
@@ -48,20 +54,18 @@ DELETE FROM posts;
 
 
 CREATE TABLE likes_dislikes (
-    user_id TEXT NOT NULL REFERENCES users (id),
-    post_id TEXT NOT NULL REFERENCES posts (id),
+    user_id TEXT NOT NULL REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    post_id TEXT NOT NULL REFERENCES posts (id) ON UPDATE CASCADE ON DELETE CASCADE,
     like INTEGER NOT NULL,
-    PRIMARY KEY (user_id, post_id),
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (post_id) REFERENCES posts (id)
+    PRIMARY KEY (user_id, post_id)
 );
 
-DROP TABLE IF EXISTS likes_dislikes;
+-- DROP TABLE IF EXISTS likes_dislikes;
 
 INSERT INTO likes_dislikes (user_id, post_id, like)
-VALUES ('u001', '1', 1), 
-       ('u002', '1', -1),
-       ('u003', '2', 1); 
+VALUES ('u001', 'p001', 1), 
+       ('u002', 'p001', -1),
+       ('u003', 'p002', 1); 
 
 
 SELECT * FROM likes_dislikes;
