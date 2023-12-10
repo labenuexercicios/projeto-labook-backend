70894e7614d37bed9dcd2edff117fd12632cbc28
Re-Santos <renata.prog@gmail.com>
2023-12-10 11:28:55 -0300

    poo

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,25 @@
 
-export type TUser = {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-    role: string;
-    created_at: string;
-  }
-  
-export type TPost = {
-    id: string;
-    creator_id: string;
-    content: string;
-    likes: number;
-    dislikes: number;
-    created_at: string;
-    updated_at: string;
-  }
-  
-export type TLikeDislike = {
+export interface TUserDB {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  created_at: string;
+}
+
+export interface TPostDB {
+  id: string;
+  creator_id: string;
+  content: string;
+  likes: number;
+  dislikes: number;
+  created_at: string;
+  updated_at: string;
+}
+
+ 
+export interface TLikeDislikeDB  {
     user_id: string;
     post_id: string;
     like: number;
